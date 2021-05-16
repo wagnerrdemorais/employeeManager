f82ddf09e5be20af5cb87d992971f36e8422b120
Wagner Rodakevicz de Morais <wagnerrdemorais@gmail.com>
2021-05-16 12:34:59 -0300

    RepoStart - Backend App

---
+++ src/main/java/com/wagnerrdemorais/employeemanager/repo/EmployeeRepo.java
@@ -0,0 +1,15 @@
+package com.wagnerrdemorais.employeemanager.repo;
+
+import com.wagnerrdemorais.employeemanager.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+
+    void deleteEmployeeById(Long id);
+
+    Optional<Employee> findEmployeeById(Long id);
+}
