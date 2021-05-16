0a077160ed47e6d38460a6907ca6efca3d611a94
Wagner Rodakevicz de Morais <wagnerrdemorais@gmail.com>
2021-05-16 13:45:44 -0300

    front-end added, cors configured on backend

--- src/main/java/com/wagnerrdemorais/employeemanager/EmployeemanagerApplication.java
+++ src/main/java/com/wagnerrdemorais/employeemanager/EmployeemanagerApplication.java
@@ -2,12 +2,52 @@ package com.wagnerrdemorais.employeemanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import static java.util.Arrays.asList;
 
 @SpringBootApplication
 public class EmployeemanagerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EmployeemanagerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeemanagerApplication.class, args);
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setAllowedOriginPatterns(asList("http://localhost:4200"));
+
+        corsConfiguration.setAllowedHeaders(asList(
+                "Origin",
+                "Access-ontroll-Allow-Origin",
+                "Content-Type",
+                "Accept",
+                "Authorization",
+                "Origin, Accept",
+                "X-Request-With",
+                "Access-Controll-Request-Method",
+                "Access-Controll-Request-Headers"
+        ));
+
+        corsConfiguration.setExposedHeaders(asList(
+                "Origin",
+                "Content-Type",
+                "Accept",
+                "Authorization",
+                "Access-Control-Allow-Origin",
+                "Access-Control-Allow-Credentials"
+        ));
+
+        corsConfiguration.setAllowedMethods(asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        UrlBasedCorsConfigurationSource urlBasedCorsConfiguratinSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfiguratinSource.registerCorsConfiguration("/**", corsConfiguration);
+
+        return new CorsFilter(urlBasedCorsConfiguratinSource);
+    }
 
 }
